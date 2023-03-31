ca87f072f9a65eaf50bdbab9e11c6f1888b24281
jamyoucn00 <mjamoun@youcan.shop>
2023-03-31 11:57:07 +0000

    update

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -1,383 +1,43 @@
-/*
-cartItems looking like this:
-[
-    {
-        "id": "1387ca37-31b3-4a58-9d15-56bceec5c938",
-        "quantity": 1,
-        "price": 12,
-        "extra_fields": [],
-        "productVariant": {
-            "id": "8d9387da-f449-4f6b-abd5-400d2ca708ec",
-            "variations": {
-                "red": "test",
-                "size": "xl"
-            },
-            "options": [
-                "red",
-                "size"
-            ],
-            "values": [
-                "test",
-                "xl"
-            ],
-            "price": 12,
-            "compare_at_price": 99,
-            "weight": null,
-            "inventory": 0,
-            "is_default": false,
-            "image": {
-                "name": null,
-                "url": null
-            },
-            "product": {
-                "id": "def2c456-0164-436e-a23c-9d0ae15c2c45",
-                "name": "فستان طبقات بزهور",
-                "slug": "fstan-tbkat-bzhor-5",
-                "public_url": null,
-                "thumbnail": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/products/NAAk8aBnDkxczkuGmfqDN1p2szZlweY3tioGxj8v_md.png",
-                "description": "<p><a href=\"https://seller-area.youcan.shop/admin/products/b01a3054-7105-4a12-aaff-c9d9aaa45f48/edit\" style='box-sizing: border-box; color: rgb(14, 100, 180); text-decoration: none; background-color: rgb(255, 255, 255); font-weight: 600; display: inline-block; max-width: 170px; font-family: Mona-Sans, -apple-system, \"system-ui\", Lato, DroidKuffi, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;'>فستان طبقات بزهور</a></p>",
-                "price": 12,
-                "compare_at_price": 99,
-                "has_variants": true,
-                "variants_count": 4,
-                "variant_options": [
-                    {
-                        "name": "red",
-                        "type": 4,
-                        "values": [
-                            "test",
-                            "test2"
-                        ]
-                    },
-                    {
-                        "name": "size",
-                        "type": 1,
-                        "values": [
-                            "xl",
-                            "xxl"
-                        ]
-                    }
-                ],
-                "track_inventory": false,
-                "you_save_amount": 87,
-                "meta": {
-                    "title": "فستان طبقات بزهور",
-                    "description": "فستان طبقات بزهور",
-                    "images": []
-                },
-                "created_at": "2023-03-28T14:57:27+00:00",
-                "has_related_products": false,
-                "related_products": [],
-                "product_settings": {
-                    "enable_reviews": false,
-                    "skip_to_checkout": false,
-                    "sticky_on_mobile": false,
-                    "sticky_on_desktop": false,
-                    "read_more": false,
-                    "enable_facebook_share": false,
-                    "enable_twitter_share": false,
-                    "enable_whatssap_share": false,
-                    "enable_product_quantity_selector": true,
-                    "related_products_section": false,
-                    "direct_add_to_cart": false,
-                    "cart_text": "Add to cart",
-                    "sections": [
-                        {
-                            "key": "title",
-                            "show": true
-                        },
-                        {
-                            "key": "preview",
-                            "show": true
-                        },
-                        {
-                            "key": "price",
-                            "show": true
-                        },
-                        {
-                            "key": "variants",
-                            "show": true
-                        },
-                        {
-                            "key": "scarcity",
-                            "show": true
-                        },
-                        {
-                            "key": "visitors",
-                            "show": true
-                        },
-                        {
-                            "key": "countdown",
-                            "show": true
-                        },
-                        {
-                            "key": "description",
-                            "show": true
-                        },
-                        {
-                            "key": "express-checkout",
-                            "show": true
-                        },
-                        {
-                            "key": "add_to_cart",
-                            "show": true
-                        }
-                    ],
-                    "visitors": {
-                        "max": 10,
-                        "min": 5
-                    },
-                    "fakeStock": {
-                        "max": 10,
-                        "min": 5
-                    },
-                    "time": {
-                        "days": 0,
-                        "hours": 1,
-                        "minutes": 0,
-                        "seconds": 0
-                    },
-                    "style": {
-                        "padding": {
-                            "top": 30,
-                            "bottom": 30
-                        },
-                        "background": {
-                            "color": "#ffffff"
-                        },
-                        "text": {
-                            "color": "#000000"
-                        },
-                        "link": {
-                            "color": "#803160"
-                        },
-                        "title": {
-                            "color": "#000000"
-                        },
-                        "price": {
-                            "before": {
-                                "color": "#747474"
-                            },
-                            "after": {
-                                "color": "#803160"
-                            }
-                        },
-                        "addToCart": {
-                            "text": {
-                                "color": "#ffffff"
-                            },
-                            "background": {
-                                "color": "#ff0000"
-                            },
-                            "border": {
-                                "color": "transparent"
-                            },
-                            "hover": {
-                                "text": {
-                                    "color": "#ffffff"
-                                },
-                                "background": {
-                                    "color": "#ff0000"
-                                },
-                                "border": {
-                                    "color": "transparent"
-                                }
-                            }
-                        },
-                        "quantityButtons": {
-                            "text": {
-                                "color": "#333333"
-                            },
-                            "background": {
-                                "color": "#f0f0f0"
-                            },
-                            "border": {
-                                "color": "#f0f0f0"
-                            },
-                            "hover": {
-                                "text": {
-                                    "color": "#333333"
-                                },
-                                "background": {
-                                    "color": "#f0f0f0"
-                                },
-                                "border": {
-                                    "color": "f0f0f0"
-                                }
-                            }
-                        },
-                        "primary": {
-                            "color": "#803160"
-                        },
-                        "secondary": {
-                            "color": "#ff0000"
-                        },
-                        "option": {
-                            "border": {
-                                "color": "#000000"
-                            }
-                        }
-                    },
-                    "cart_safe_badges": null,
-                    "one_page_checkout": true
-                },
-                "images": [
-                    {
-                        "id": "21c5f8f1-ab9a-4a6b-a201-2a5fc4420027",
-                        "name": "stores/c611907409af35137b71c51dab490ec2/products/NAAk8aBnDkxczkuGmfqDN1p2szZlweY3tioGxj8v.png",
-                        "type": 1,
-                        "url": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/products/NAAk8aBnDkxczkuGmfqDN1p2szZlweY3tioGxj8v.png",
-                        "order": 1,
-                        "variations": {
-                            "original": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/products/NAAk8aBnDkxczkuGmfqDN1p2szZlweY3tioGxj8v.png",
-                            "sm": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/products/NAAk8aBnDkxczkuGmfqDN1p2szZlweY3tioGxj8v_sm.png",
-                            "md": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/products/NAAk8aBnDkxczkuGmfqDN1p2szZlweY3tioGxj8v_md.png",
-                            "lg": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/products/NAAk8aBnDkxczkuGmfqDN1p2szZlweY3tioGxj8v_lg.png"
-                        }
-                    }
-                ],
-                "variants": [
-                    {
-                        "id": "3697bbba-4963-4275-8ffa-2e27ab2c9bce",
-                        "variations": {
-                            "red": "test2",
-                            "size": "xxl"
-                        },
-                        "options": [
-                            "red",
-                            "size"
-                        ],
-                        "values": [
-                            "test2",
-                            "xxl"
-                        ],
-                        "price": 12,
-                        "compare_at_price": 99,
-                        "weight": null,
-                        "inventory": 0,
-                        "is_default": false,
-                        "image": {
-                            "name": null,
-                            "url": null
-                        }
-                    },
-                    {
-                        "id": "86dd7994-cf6f-4c78-b5fd-2c49fd767032",
-                        "variations": {
-                            "red": "test2",
-                            "size": "xl"
-                        },
-                        "options": [
-                            "red",
-                            "size"
-                        ],
-                        "values": [
-                            "test2",
-                            "xl"
-                        ],
-                        "price": 12,
-                        "compare_at_price": 99,
-                        "weight": null,
-                        "inventory": 0,
-                        "is_default": false,
-                        "image": {
-                            "name": null,
-                            "url": null
-                        }
-                    },
-                    {
-                        "id": "8c5e0b95-befc-48ab-b046-1e31a8b8769e",
-                        "variations": {
-                            "red": "test",
-                            "size": "xxl"
-                        },
-                        "options": [
-                            "red",
-                            "size"
-                        ],
-                        "values": [
-                            "test",
-                            "xxl"
-                        ],
-                        "price": 12,
-                        "compare_at_price": 99,
-                        "weight": null,
-                        "inventory": 0,
-                        "is_default": false,
-                        "image": {
-                            "name": null,
-                            "url": null
-                        }
-                    },
-                    {
-                        "id": "8d9387da-f449-4f6b-abd5-400d2ca708ec",
-                        "variations": {
-                            "red": "test",
-                            "size": "xl"
-                        },
-                        "options": [
-                            "red",
-                            "size"
-                        ],
-                        "values": [
-                            "test",
-                            "xl"
-                        ],
-                        "price": 12,
-                        "compare_at_price": 99,
-                        "weight": null,
-                        "inventory": 0,
-                        "is_default": false,
-                        "image": {
-                            "name": null,
-                            "url": null
-                        }
-                    }
-                ],
-                "categories": [
-                    {
-                        "name": "أحدث ما وصلنا",
-                        "image": "stores/c611907409af35137b71c51dab490ec2/categories/cHHfBPvr3gDFWc9oPg8a681TeYWqUlfZJ2Pm8eG6.png",
-                        "slug": "fsatyn-zfaf-1",
-                        "show_on_collection": true,
-                        "public_url": null,
-                        "description": "فساتين زفاف",
-                        "has_parent": false,
-                        "parent_id": null,
-                        "is_default": false,
-                        "images": {
-                            "original": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/categories/cHHfBPvr3gDFWc9oPg8a681TeYWqUlfZJ2Pm8eG6.png",
-                            "sm": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/categories/cHHfBPvr3gDFWc9oPg8a681TeYWqUlfZJ2Pm8eG6_sm.png",
-                            "md": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/categories/cHHfBPvr3gDFWc9oPg8a681TeYWqUlfZJ2Pm8eG6_md.png",
-                            "lg": "https://cdn.youcan.shop/stores/c611907409af35137b71c51dab490ec2/categories/cHHfBPvr3gDFWc9oPg8a681TeYWqUlfZJ2Pm8eG6_lg.png"
-                        },
-                        "meta": {
-                            "title": "أحدث ما وصلنا",
-                            "description": "أحدث ما وصلنا sdfdsf",
-                            "images": [],
-                            "images_urls": []
-                        }
-                    }
-                ]
-            }
-        }
-    }
-]
-*/
 async function setupCartDrawer() {
-  function createCartItemHtml({ productVariant }) {
-    console.log(productVariant)
-    if (cartItem) {
+  const cartDrawerContentEl = document.querySelector('.cart-drawer .content')
+  const cartDrawerTotalEl = document.querySelector('.cart-drawer .total')
+
+  /**
+   * basing on the cartDrawerTotalEl content and replacing only the number and keeping the currency symbol
+   * @param {Number} price
+   */
+  function money(price) {
+    return cartDrawerTotalEl.innerHTML.replace(/(\d+\.?\d*)/, price)
+  }
+
+  /**
+   * Return a product variant HTML string
+   * @param {cartItem} - the cart item
+   * @returns {string} - HTML content
+   */
+  function createCartItemHtml(cartItem) {
+    const { productVariant, quantity } = cartItem
+
+    console.log(productVariant, quantity)
+    if (productVariant) {
       return `
         <div class="cart-item">
-          <div class="cart-item-image">
-            <img src="${productVariant.image.url || productVariant.product.images[0]}" alt="${productVariant.title}">
+          <div class="image">
+            <img src="${productVariant.image.url || productVariant.product.images[0].url}" />
           </div>
-          <div class="cart-item-info">
-            <div class="cart-item-title">${productVariant.product.title}</div>
-            <div class="cart-item-quantity">Quantity: ${productVariant.quantity}</div>
-            <div class="cart-item-price">Price: ${productVariant.price}</div>
+          <div class="info">
+            <div class="title">${productVariant.product.name || ''}</div>
+            <div class="quantity">Quantity: ${quantity || 0}</div>
+            <div class="variants">
+              ${Object.keys(productVariant.variations)
+                      .map(key => `
+                        <div>${key}: ${productVariant.variations[key]}</div>
+                      `)
+                      .join('')
+              }
+            </div>
           </div>
+          <div class="price">Price: ${money(productVariant.price * quantity)}</div>
         </div>
       `
     }
@@ -388,13 +48,9 @@ async function setupCartDrawer() {
   try {
     const cartObject = await youcanjs.cart.fetch()
     const cartItems = cartObject.items
-    const cartDrawerContentEl = document.querySelector('.cart-drawer .content')
-    const cartDrawerTotalEl = document.querySelector('.cart-drawer .total')
 
     cartDrawerContentEl.innerHTML = cartItems.map(createCartItemHtml).join('')
-
-    // Replacing only the number in the string and keeping the currency symbol
-    cartDrawerTotalEl.innerHTML = cartDrawerTotalEl.innerHTML.replace(/(\d+\.?\d*)/, cartObject.total)
+    cartDrawerTotalEl.innerHTML = money(cartObject.total)
   } catch (error) {
     console.error(error)
   }

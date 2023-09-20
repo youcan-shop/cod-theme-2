51acbaf35b1cc852c3659a29120e4a1cd43cacd2
adildev101 <adil@youcan.shop>
2023-09-20 16:10:31 +0100

    update date format

--- assets/reviews.js
+++ assets/reviews.js
@@ -10,7 +10,7 @@ function convertDate() {
     const day = originalDate.getDate().toString().padStart(2, '0');
     const month = (originalDate.getMonth() + 1).toString().padStart(2, '0');
     const year = originalDate.getFullYear().toString();
-    const formattedDate = `${day}.${month}.${year}`;
+    const formattedDate = `${day}/${month}/${year}`;
     date.textContent = formattedDate;
   });
 }
